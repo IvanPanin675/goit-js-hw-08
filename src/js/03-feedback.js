51273e59de87f27f57d35dab9dd4eb27516e6070
IvanPanin675 <jovanni.94.van@gmail.com>
2022-11-21 18:29:23 +0200

    gotovo

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,75 +3,50 @@ import throttle from "lodash.throttle";
 const refs = {
   form: document.querySelector('.feedback-form')
 }
-const LOCALSTORAGE_KEY = "feedback-form-state";
+
 const formDate = {};
+const LOCALSTORAGE_KEY = "feedback-form-state";
 
+(function () {
+  if (localStorage.getItem(LOCALSTORAGE_KEY, JSON.stringify(formDate))) {
+    let localEmail = localStorage.getItem(LOCALSTORAGE_KEY);
+    localEmail = JSON.parse(localEmail);
+    refs.form.email.value = localEmail.email;
+    refs.form.message.value = localEmail.message;
+    console.log(localEmail);
+  }
+}) ();
 
-initForm();
-// texteriaMessage(formDate);
 refs.form.addEventListener('submit', onButtonClick);
-// refs.form.addEventListener('input', onInputClick);
-// refs.form.addEventListener('input', throttle(onEmail, 500));
-// refs.form.addEventListener('input', throttle(onMessage, 500));
+refs.form.addEventListener('input', throttle(onEmail, 500));
+refs.form.addEventListener('input', throttle(onMessage, 500));
 
 function onButtonClick(e) {
 e.preventDefault();
 if (refs.form.email.value === "" || refs.form.message.value  === "") {
   alert('всі поля повинні бути заповнені!');
 } else {
-  const formData = new FormData(refs.form);
-  formData.forEach(data => console.log(data));
-  
-  // localStorage.removeItem(LOCALSTORAGE_KEY);
-  // localStorage.removeItem('Email');
-  // console.log(formDate);
-  // e.currentTarget.reset();
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  e.currentTarget.reset();
 };
 };
 
 refs.form.addEventListener('change', evt => {
-  formDate[e.target.name] = e.target.value;
-  localStorage.setItem('formDate', JSON.stringify(formDate))
+  if (!(refs.form.email.value === "" || refs.form.message.value === "")) {
+    formDate[refs.form.email.name] = refs.form.email.value;
+    formDate[refs.form.message.name] = refs.form.message.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDate));
+    console.log(formDate);
+  } 
 });
 
-function initForm() {
-  let persistedText = localStorage.getItem(formDate);
-  if (persistedText) {
-    persistedText = JSON.parse(persistedText);
-    console.log(persistedText);
-  }
-}
+function onEmail(e) {
+formDate[refs.form.email.name] = refs.form.email.value;
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDate));
+};
 
-// function onInputClick(e) {
-// formDate[e.target.name] = e.target.value;
-// // console.log(formDate);
-// }
-
-// function onEmail(e) {
-// const email = JSON.stringify(formDate[e.target.name]);
-// localStorage.setItem('Email', email);
-
-// };
-
-// function onMessage(e) {
-//   const message = JSON.stringify(formDate[e.target.name]);
-//   localStorage.setItem(LOCALSTORAGE_KEY, message);
-//   // console.log(message)
-// };
-
-// function texteriaMessage() {
-  
-//  const saveMessage = localStorage.getItem(LOCALSTORAGE_KEY);
-//  const saveEmail = localStorage.getItem('Email');
-
-//  if (saveMessage) {
-//  refs.form.message.value = JSON.parse(saveMessage);
-//  formDate.message = refs.form.message.value;
-//  }
-
-//  if (saveEmail) {
-// refs.form.email.value = JSON.parse(saveEmail);
-// formDate.email = refs.form.email.value;
-//  }
- 
-// }
+function onMessage(e) {
+  formDate[refs.form.message.name] = refs.form.message.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formDate));
+};
